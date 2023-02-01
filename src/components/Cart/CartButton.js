d9a4a7f0949e2502daa0ba6c76b91a6c2c333d39
Bozhidar Gorchev <bgorchev@aiopsgroup.com>
2023-02-01 11:10:40 +0200

    add react redux training project

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+
+import classes from "./CartButton.module.css";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuontity = useSelector((state) => state.cart.totalQuantity);
+ 
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuontity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
